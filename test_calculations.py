6d157b95a9741ec728cf52653b9110277320aa69
Ubuntu <ubuntu@ip-172-31-23-56.eu-west-1.compute.internal>
2023-11-26 09:45:37 +0000

    Modification pour flake8

--- test_calculations.py
+++ test_calculations.py
@@ -1,16 +1,12 @@
 # test_calculations.py
 
-import pytest
 from calculs import addition, soustraction, multiplication, coucou
 
 def test_calc_addition():
     assert addition(2, 4) == 6
-
 def test_calc_substraction():
     assert soustraction(2, 4) == -2
-
 def test_calc_multiply():
     assert multiplication(2, 4) == 8
-
 def test_coucou():
     assert coucou() == 'hello'
