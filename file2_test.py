61d50331953bed02b1ec3bb71df57d6ff2af1cde
Ubuntu <ubuntu@ip-172-31-23-56.eu-west-1.compute.internal>
2023-11-26 09:58:41 +0000

    dur dur et Respect convention PEP8

--- file2_test.py
+++ file2_test.py
@@ -5,17 +5,14 @@ def test_calc_addition():
     # Test de la fonction pour le résultat de 2 + 4
     output = 2 + 4
     assert output == 6
-
 def test_calc_substraction():
     # Test de la fonction pour le résultat de 2 - 4
     output = 2 - 4
     assert output == -2
-
 def test_calc_multiply():
     # Test de la fonction pour le résultat de 2 * 4
     output = 2 * 4
     assert output == 8
-
 def test_coucou():
     # Test de la fonction pour vérifier si le résultat est 'hello'
     output = 'hello'
